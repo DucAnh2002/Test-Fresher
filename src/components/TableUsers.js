e08e7562558b12d4948d8b975ec2213b7d6869a9
DucAnh2002 <haducanh02012002@gmail.com>
2025-01-18 23:10:02 +0700

    Axios Customiize

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -15,8 +15,9 @@ const TableUsers = (props) => {
 
 const getUsers = async () => {
     let res = await fetchAllUser();
-    if ( res && res.data && res.data.data) {
-        setListUsers(res.data.data)
+    console.log(">>> check new res: ", res)
+    if ( res && res.data ) {
+        setListUsers(res.data)
     }
     console.log(">>> check res ", res)
 }
