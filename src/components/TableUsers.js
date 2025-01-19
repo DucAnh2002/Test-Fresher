4cddcdddfe4782225950cb95acb032a912c792b3
DucAnh2002 <haducanh02012002@gmail.com>
2025-01-19 11:38:15 +0700

    Refactor PaginationPagination

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,29 +1,42 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
-
+import ReactPaginate from 'react-paginate';
 
 const TableUsers = (props) => {
 
     const [listUsers, setListUsers] = useState ([]);
+    const [totalUsers, setTotalUsers] = useState (0);
+    const [totalPages, setTotalPages] = useState (0);
+
+
 
     useEffect (() => {
         // call apis
         // dry
-        getUsers ();
-    },[])
+        getUsers (2);
+    }, [])
 
-const getUsers = async () => {
-    let res = await fetchAllUser();
-    console.log(">>> check new res: ", res)
+const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
     if ( res && res.data ) {
-        setListUsers(res.data)
+      console.log(res)
+      setTotalUsers(res.total)
+      setListUsers(res.data)
+      setTotalPages(res.total_pages)
     }
-    console.log(">>> check res ", res)
 }
 
-    console.log(listUsers)
+const handlePageClick = (event) => {
+  console.log(">>> event lib :", event)
+  getUsers(+ event.selected + 1)
+}
+
+
+
     return (
+   
+
 //         avatar
 // : 
 // "https://reqres.in/img/faces/1-image.jpg"
@@ -39,6 +52,7 @@ const getUsers = async () => {
 // last_name
 // : 
 // "Bluth"
+          <>
             <Table striped bordered hover>
               <thead>
                 <tr>
@@ -63,6 +77,26 @@ const getUsers = async () => {
                 }
               </tbody>
             </Table>
+            <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={totalPages}
+            previousLabel="< previous"
+            
+            pageClassName='page-item'
+            pageLinkClassName='page-link'
+            previousClassName='page-item'
+            previousLinkClassName='page-link'
+            nextClassName='page-item'
+            nextLinkClassName='page-link'
+            breakClassName='page-item'
+            breakLinkClassName='page-link'
+            containerClassName='pagination'
+            activeClassName='active'
+            />
+          </>
           );
         }
 export default TableUsers
